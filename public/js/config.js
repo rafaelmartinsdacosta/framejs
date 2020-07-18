be93ccc52f8c0eeb44bdd70a7e5d05f4ed076dcf
rafaelmartinsdacosta <rafael_martins_da_costa@outlook.com>
2020-07-18 09:47:41 -0300

    Formatação de código

--- public/js/config.js
+++ public/js/config.js
@@ -1,14 +1,14 @@
  // Your web app's Firebase configuration
  let firebaseConfig = {
-  apiKey: "AIzaSyDooajfXGe4-m6S42MKZLhB2zIz9rmJ2Ns",
-  authDomain: "framejs.firebaseapp.com",
-  databaseURL: "https://framejs.firebaseio.com",
+  apiKey: "-xxxx",
+  authDomain: "xxx.firebaseapp.com",
+  databaseURL: "https://xxxx.firebaseio.com",
   projectId: "framejs",
-  storageBucket: "framejs.appspot.com",
-  messagingSenderId: "145993703858",
-  appId: "1:145993703858:web:a84ecc7e267b96447704ab",
-  measurementId: "G-NQ1ETC7RK6"
+  storageBucket: "xxxx.appspot.com",
+  messagingSenderId: "xxxx",
+  appId: "1:xxx:web:xxx",
+  measurementId: "xxxx"
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
